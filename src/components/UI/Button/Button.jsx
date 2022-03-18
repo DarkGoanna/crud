2b1faac265170af21adea72bcf870d78c79ecb77
Artur Savchuk <a.savchuk@brambligtech.com>
2022-03-18 11:43:44 +0200

    add button

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './Button.module.scss'
+
+const Button = ({children, ...props}) => {
+  return (
+    <button className={style.button} {...props}>{children}</button>
+  )
+}
+export default Button
